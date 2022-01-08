f3311e777cf378e5081e4f603a2976ebfda83801
xovest <79008917+xovest@users.noreply.github.com>
2022-01-08 14:33:22 +0200

    tests

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,18 @@
+const DappToken = artifacts.require("DappToken");
+const DaiToken = artifacts.require("DaiToken");
 const TokenFarm = artifacts.require("TokenFarm");
 
-module.exports = function(deployer) {
-  deployer.deploy(TokenFarm);
+module.exports = async function(deployer, network, accounts) {
+  await deployer.deploy(DaiToken); //Mock DAI
+  const daiToken = await DaiToken.deployed();
+
+  await deployer.deploy(DappToken); //Dapp
+  const dappToken = await DappToken.deployed();
+
+  await deployer.deploy(TokenFarm, dappToken.address, daiToken.address); //TokenFarm
+  const tokenFarm = await TokenFarm.deployed();
+
+  await dappToken.transfer(tokenFarm.address, '1000000000000000000000000'); //transfer 1mil
+
+  await daiToken.transfer(accounts[1], '100000000000000000000')
 };
